8cd6600fba2e816ebed6f6b800c4ca89ec65e503
savan <kansagrasavan108@gmail.com>
2022-06-10 09:45:30 +0530

    Initial commit to starter project

---
+++ src/main/java/com/greentree/learning/Transport1Application.java
@@ -0,0 +1,13 @@
+package com.greentree.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Transport1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Transport1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/greentree/learning/Transport1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greentree.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Transport1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
